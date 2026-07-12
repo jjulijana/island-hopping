a86f636db1745f29ed028512efaf511b3c354f78
Julijana Jevtic <mi251131@alas.matf.bg.ac.rs>
2026-07-12 18:50:09 +0200

    Add reading input from file and add example

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "solver.h"
 
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 
@@ -7,22 +8,40 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int m;
-    if (!(std::cin >> m)) {
+    std::ifstream inputFile("example.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Failed to open input file: example.txt\n";
+        return 1;
+    }
+
+    int caseCount;
+    if (!(inputFile >> caseCount)) {
         return 0;
     }
 
-    std::vector<Point> points;
-    points.reserve(m);
+    for (int caseIndex = 0; caseIndex < caseCount; ++caseIndex) {
+        int m;
+        if (!(inputFile >> m)) {
+            std::cerr << "Unexpected end of input while reading case " << caseIndex + 1 << '\n';
+            return 1;
+        }
 
-    for (int i = 0; i < m; ++i) {
-        double x, y;
-        std::cin >> x >> y;
-        points.push_back({x, y, i});
+        std::vector<Point> points;
+        points.reserve(m);
+
+        for (int i = 0; i < m; ++i) {
+            double x, y;
+            if (!(inputFile >> x >> y)) {
+                std::cerr << "Unexpected end of input while reading point " << i + 1
+                          << " in case " << caseIndex + 1 << '\n';
+                return 1;
+            }
+            points.push_back({x, y, i});
+        }
+
+        SolverResult result = Solver::solve(points);
+        std::cout << std::fixed << std::setprecision(10) << result.mstLength << '\n';
     }
 
-    SolverResult result = Solver::solve(points);
-
-    std::cout << std::fixed << std::setprecision(10) << result.mstLength << '\n';
     return 0;
 }
